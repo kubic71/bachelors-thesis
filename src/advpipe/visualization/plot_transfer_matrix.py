148aae699529a8ffda513b02a76e0d32ea2e9347
Jakub Hejhal <kubic71@gmail.com>
2021-07-11 19:34:23 +0200

    some nice transfer heatmaps

---
+++ src/advpipe/visualization/plot_transfer_matrix.py
@@ -0,0 +1,92 @@
+from utils import gather_results
+import matplotlib.pyplot as plt
+from PIL import Image
+import seaborn as sns
+import pandas as pd
+from typing import Any
+
+
+def plot_matrix(data: pd.DataFrame, plot_fn: str, title: str = "") -> Any:
+    data_rows = []
+
+    for surrogate in data["surrogate"].unique():
+        for target in data["target"].unique():
+            exp_df = data.query(f"surrogate == '{surrogate}' and target == '{target}'")
+            foolrate = len(exp_df.query("target_loss < 0")) / len(exp_df)
+
+            # print(surrogate, "to", target, "\t", foolrate)
+            data_rows.append({"surrogate":surrogate, "target":target, "foolrate": foolrate})
+
+    
+    pd_matrix = pd.DataFrame(data_rows).pivot(index="surrogate", columns="target", values="foolrate")
+
+    plt.figure()
+    sns.set_theme(style="darkgrid")
+    ax= sns.heatmap(pd_matrix, fmt='.1%', annot=True, xticklabels=True, yticklabels=True, square=True, cbar=False, annot_kws={"fontsize":8})
+    ax.set(xlabel="Target")
+    ax.set(ylabel="Surrogate")
+    
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0, fontsize=8)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, fontsize=8, rotation_mode='anchor', ha='right')
+    # dist_plot.ax.set_title("APGD")
+    plt.suptitle(title, fontsize=12)
+    plt.tight_layout()
+    plt.savefig(plot_fn)
+    return ax
+
+
+def generate_all_heatmaps() -> None:
+    # format: (title, plot_filename, source_results_dir)
+    plot_details = [
+        #### APGD L2 ####
+
+        # output-mapping margin
+        ("APGD, margin output mapping, n_images=500, n_iters=25, l2_norm=3", "plots/transfer_attacks/transfer_heatmap/apgd_l2_margin_n_iters_25_eps_3.png", "../results/net_crossproduct_apgd_surrogate_outputmap_margin/eps_3"),
+        ("APGD, margin output mapping, n_images=500, n_iters=25, l2_norm=10", "plots/transfer_attacks/transfer_heatmap/apgd_l2_margin_n_iters_25_eps_10.png", "../results/net_crossproduct_apgd_surrogate_outputmap_margin/eps_10"),
+        ("APGD, margin output mapping, n_images=500, n_iters=25, l2_norm=20", "plots/transfer_attacks/transfer_heatmap/apgd_l2_margin_n_iters_25_eps_20.png", "../results/net_crossproduct_apgd_surrogate_outputmap_margin/eps_20"),
+
+
+        # output-mapping logits
+        ("APGD, logits output mapping, n_images=500, n_iters=25, l2_norm=3", "plots/transfer_attacks/transfer_heatmap/apgd_l2_logits_n_iters_25_eps_3.png", "../results/net_crossproduct_apgd_surrogate_outputmap_logits/eps_3"),
+        ("APGD, logits output mapping, n_images=500, n_iters=25, l2_norm=10", "plots/transfer_attacks/transfer_heatmap/apgd_l2_logits_n_iters_25_eps_10.png", "../results/net_crossproduct_apgd_surrogate_outputmap_logits/eps_10"),
+        ("APGD, logits output mapping, n_images=500, n_iters=25, l2_norm=20", "plots/transfer_attacks/transfer_heatmap/apgd_l2_logits_n_iters_25_eps_20.png", "../results/net_crossproduct_apgd_surrogate_outputmap_logits/eps_20"),
+
+        #### FGSM ####
+        # output-mapping logits
+        ("FGSM, logits output mapping, n_images=500, linf_norm=0.01", "plots/transfer_attacks/transfer_heatmap/fgsm_logits_eps_0.010.png", "../results/net_crossproduct_fgsm/output_map_logits/eps_0.01"),
+        ("FGSM, logits output mapping, n_images=500, linf_norm=0.025", "plots/transfer_attacks/transfer_heatmap/fgsm_logits_eps_0.025.png", "../results/net_crossproduct_fgsm/output_map_logits/eps_0.025"),
+        ("FGSM, logits output mapping, n_images=500, linf_norm=0.05", "plots/transfer_attacks/transfer_heatmap/fgsm_logits_eps_0.050.png", "../results/net_crossproduct_fgsm/output_map_logits/eps_0.05"),
+        ("FGSM, logits output mapping, n_images=500, linf_norm=0.1", "plots/transfer_attacks/transfer_heatmap/fgsm_logits_eps_0.100.png", "../results/net_crossproduct_fgsm/output_map_logits/eps_0.1"),
+        ("FGSM, logits output mapping, n_images=500, linf_norm=0.2", "plots/transfer_attacks/transfer_heatmap/fgsm_logits_eps_0.200.png", "../results/net_crossproduct_fgsm/output_map_logits/eps_0.2"),
+
+        # output-mapping margin
+        ("FGSM, margin output mapping, n_images=500, linf_norm=0.01", "plots/transfer_attacks/transfer_heatmap/fgsm_margin_eps_0.010.png", "../results/net_crossproduct_fgsm/output_map_organism_margin/eps_0.01"),
+        ("FGSM, margin output mapping, n_images=500, linf_norm=0.025", "plots/transfer_attacks/transfer_heatmap/fgsm_margin_eps_0.025.png", "../results/net_crossproduct_fgsm/output_map_organism_margin/eps_0.025"),
+        ("FGSM, margin output mapping, n_images=500, linf_norm=0.05", "plots/transfer_attacks/transfer_heatmap/fgsm_margin_eps_0.050.png", "../results/net_crossproduct_fgsm/output_map_organism_margin/eps_0.05"),
+        ("FGSM, margin output mapping, n_images=500, linf_norm=0.1", "plots/transfer_attacks/transfer_heatmap/fgsm_margin_eps_0.100.png", "../results/net_crossproduct_fgsm/output_map_organism_margin/eps_0.1"),
+        ("FGSM, margin output mapping, n_images=500, linf_norm=0.2", "plots/transfer_attacks/transfer_heatmap/fgsm_margin_eps_0.200.png", "../results/net_crossproduct_fgsm/output_map_organism_margin/eps_0.2"),
+        # ("APGD demo", "apgd_demo.png", "../results/net_crossproduct_apgd_demo"),
+     ]
+
+    for title, plot_fn, res_dir in plot_details:
+        print(f"Title: {title}, plot filename: {plot_fn}, source_dir: {res_dir}")
+        df = gather_results(res_dir)
+        _ = plot_matrix(df, plot_fn, title)
+
+
+
+    # heatmap.show()
+
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--dir", required=True, type=str)
+
+# args = parser.parse_args()
+
+generate_all_heatmaps()
+# plt.show()
+# plot.figure.savefig('/tmp/test.png', transparent=True, bbox_inches='tight')
+
+
+
+
--- src/advpipe/visualization/utils.py
+++ src/advpipe/visualization/utils.py
@@ -1,13 +1,29 @@
 from __future__ import annotations
 
 import pandas as pd
+import yaml
+import os
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-    from typing import Callable, Dict
+    from typing import Callable, Dict, Optional, Sequence, Iterator
 
 
-# def list_all_results()
+def get_config(res_dir: str) -> Optional[Dict]:
+    for filename in os.listdir(res_dir):
+        if filename.endswith(".yaml"):
+            return yaml.load(filename) # type: ignore
+    return None
+
+
+def get_all_csvs(res_dir: str) -> Iterator[str]:
+    for sub_dir, _, _ in os.walk(res_dir):
+        for filename in os.listdir(sub_dir):
+            if filename.endswith(".csv"):
+                # print("getallcsv")
+                # sub_dir = sub_dir[len(res_dir) + 1:]
+                # print(res_dir, sub_dir, filename)
+                yield os.path.join(sub_dir, filename)
 
 
 def gather_results(
@@ -25,4 +41,36 @@ def gather_results(
             - dataframe_transform: Callable[[pd.DataFrame, Dict], pd.DataFrame]
                 - postprocessing for each experiment dataframe. It can also use Dict experiment config
     """
-    raise NotImplementedError
+
+
+    data_fs = []
+    for res_dir, _, _ in os.walk(results_dir):
+        config = get_config(res_dir)
+        if config is None or not experiment_filter(config):
+            continue
+    
+        print("Experiment dir: ", res_dir)
+        # we are in experiment directory
+
+        exp_pds = []
+
+        for csv_file in get_all_csvs(res_dir):
+            exp_pds.append(pd.read_csv(csv_file, delimiter="\t"))
+        
+        exp_pd = dataframe_transform(pd.concat(exp_pds), config)
+
+        # print(exp_pd)
+
+        data_fs.append(exp_pd)
+
+    return pd.concat(data_fs)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", required=True, type=str)
+
+    args = parser.parse_args()
+
+    gather_results(args.dir)
